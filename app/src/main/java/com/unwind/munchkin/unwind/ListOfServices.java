e9367867805aecdef1fc4c5c4376ca78a2081ccb
MisterMunchkin <dalmiet@gmail.com>
2017-09-15 23:22:34 +0800

    putting up static list of services

---
+++ app/src/main/java/com/unwind/munchkin/unwind/ListOfServices.java
@@ -0,0 +1,24 @@
+package com.unwind.munchkin.unwind;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Munchkin on 9/14/2017.
+ */
+
+public class ListOfServices  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.list_of_services);
+
+
+    }
+}
